0a45747e1e0b792da12ea9800b21c607ef7763d0
Pedro Guzman <pedro.grifin@gmail.com>
2020-02-06 11:14:46 +0100

    Nuevo metodo

--- src/FiguraGeometricaPGB.java
+++ src/FiguraGeometricaPGB.java
@@ -16,6 +16,10 @@ public abstract class FiguraGeometricaPGB {
 		return tipoFigura;
 	}
 
+	public double semiPerimetro() {
+		return this.perimetro() / 2;
+	}
+	
 	public void setTipoFigura(String tipoFigura) {
 		this.tipoFigura = tipoFigura;
 	}
--- src/TrianguloPGB.java
+++ src/TrianguloPGB.java
@@ -23,7 +23,5 @@ public class TrianguloPGB extends FiguraGeometricaPGB {
 		return Math.sqrt(sp * (sp-lado1) * (sp-lado2) * (sp-lado3));
 	}
 	
-	public double semiPerimetro() {
-		return this.perimetro() / 2;
-	}
+	
 }
